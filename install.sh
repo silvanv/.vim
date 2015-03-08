2dd6de5704414db7c4424f6c90e39bcb668f83af
silvanv <vogelsanger@hotmail.com>
2015-03-08 20:38:46 +0100

    install script fonts

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+if [ -e ~/.vimrc ]; then
+  echo "backup .vimrc to .vimrc_inst_bak"
+  cp ~/.vimrc ~/.vimrc_inst_bak
+  rm ~/.vimrc
+fi
+
+echo "install .vimrc"
 ln -s ~/.vim/.vimrc ~/.vimrc
+
+echo "install fonts"
+~/.vim/fonts/install.sh
 
